f24426b7481622e72bbe155f9a571e11c59a629b
Doemsdagding <164404639+Doemsdagding@users.noreply.github.com>
2024-10-13 13:44:56 +0200

    docstring gradient.py

--- gradient.py
+++ gradient.py
@@ -6,6 +6,22 @@ class gradient(object):
 
     @staticmethod
     def diagonal(*args):
+        """
+        Create a diagonal gradient texture.
+
+        This method generates a square texture with a diagonal gradient using the provided color arguments.
+        The gradient transitions smoothly between the given colors along the diagonal.
+
+        Args:
+            *args: Variable number of color tuples. Each tuple should contain 4 float values (r, g, b, a)
+               representing red, green, blue, and alpha channels respectively, each in the range [0, 1].
+
+        Returns:
+            Texture: A Kivy Texture object representing the diagonal gradient.
+
+        The size of the texture is determined by the number of color arguments provided.
+        The gradient is created using NumPy for efficient computation.
+        """
         size = len(args)
         texture = Texture.create(size=(size, size), colorfmt='rgba')
 
