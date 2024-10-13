ccae74e9f83da789e748b75f08deb5c9f95920c5
Doemsdagding <164404639+Doemsdagding@users.noreply.github.com>
2024-10-13 13:58:30 +0200

    Update reshape.py

--- reshape.py
+++ reshape.py
@@ -29,9 +29,35 @@ class MyRoot(BoxLayout):
         super(MyRoot, self).__init__()
 
     def choose_file(self):
+        """
+        Open a file chooser dialog for selecting image files.
+
+        This method launches a file chooser dialog that allows the user to select
+        either JPEG (.jpg) or PNG (.png) image files. Once a file is selected,
+        the 'selected' method of this class will be called with the chosen file(s).
+
+        The file chooser is configured with the following filters:
+        - JPEG Files (*.jpg)
+        - PNG Files (*.png)
+        """
         filechooser.open_file(on_selection=self.selected, filters=[("JPEG Files", "*.jpg"),("PNG Files", "*.png")])
 
     def selected(self, selection):
+        """
+        Process the selected image file.
+
+        This method is called when a file is selected from the file chooser.
+        It performs the following operations:
+        1. Stores the original file path.
+        2. Opens the selected image.
+        3. Resizes the image to 680x453 pixels using LANCZOS resampling.
+        4. Saves the resized image as a preview.
+        5. Updates the image source for display.
+
+        Args:
+            selection: A list containing the path of the selected file.
+                while a list is possible as user can select multiple files only the first file is used
+        """
         if selection:
             self.original_path = selection[0]
             with Image.open(selection[0]) as img:
@@ -41,6 +67,21 @@ class MyRoot(BoxLayout):
                 self.image_source = preview_path
 
     def resize_image(self):
+        """
+        Resize the original image and update the display.
+
+        This method performs the following operations:
+        1. Checks if an original image path exists.
+        2. Opens the original image.
+        3. Resizes the image to 680x453 pixels using LANCZOS resampling.
+        4. Saves the resized image, overwriting the original file.
+        5. Updates the image_source to display the resized image.
+        6. Removes the temporary preview image if it exists.
+
+        The method only executes if an original image path has been set.
+        After resizing, the original file is replaced with the resized version.
+        The temporary 'preview.png' file is deleted to clean up resources.
+        """
         if hasattr(self, 'original_path'):
             with Image.open(self.original_path) as img:
                 resized = img.resize((680, 453), Image.Resampling.LANCZOS)
